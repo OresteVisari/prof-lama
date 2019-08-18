d63a5a04af4c7372d78235b3fef48811821161fe
Philippe BENOIT <pbenoit@palo-it.com>
2019-08-18 17:26:43 +0800

    UI improvment and 1 more example

--- app/src/main/java/com/example/proflama/LaunchScreenActivity.java
+++ app/src/main/java/com/example/proflama/LaunchScreenActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -15,6 +17,10 @@ public class LaunchScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_launch_screen);
 
         lamaIcon = findViewById(R.id.lamaIcon);
--- app/src/main/java/com/example/proflama/MainActivity.java
+++ app/src/main/java/com/example/proflama/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.proflama;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import java.util.List;
@@ -16,10 +18,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        this.setContentView(R.layout.activity_main);
 
         Note defaultNote = new Note("Aimer");
-        defaultNote.setDefinition("Eprouver de l'affection, de l'amour ou de l'attachement pour quelqu'un ou quelque chose. ");
+        defaultNote.setDefinition("Eprouver de l'affection, de l'amour ou de l'attachement pour quelqu'un ou quelque chose.");
         defaultNote.setQuote("\"Aimer, ce n'est pas se regarder l'un l'autre, c'est regarder ensemble dans la même direction.\"\nAntoine De Saint-Exupéry");
 
         TextView title = findViewById(R.id.title);
@@ -28,7 +34,20 @@ public class MainActivity extends AppCompatActivity {
         TextView definition = findViewById(R.id.definition);
         definition.setText(defaultNote.getDefinition());
 
-        TextView quote = findViewById(R.id.quote);
-        quote.setText(defaultNote.getQuote());
+        //TextView quote = findViewById(R.id.quote);
+        //quote.setText(defaultNote.getQuote());
+
+
+        Note note2 = new Note("Passion");
+        note2.setDefinition("État affectif intense et irraisonné qui domine quelqu'un");
+        note2.setQuote("Nous respectons la raison, mais nous aimons nos passions.");
+
+        TextView title2 = findViewById(R.id.title2);
+        title2.setText(note2.getTitle());
+
+        TextView definition2 = findViewById(R.id.definition2);
+        definition2.setText(note2.getDefinition());
+
+
     }
 }
