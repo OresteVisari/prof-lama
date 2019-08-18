2d6e596a57c8de0d295eabfdd1ee48244b0adf90
Philippe BENOIT <pbenoit@palo-it.com>
2019-08-18 20:11:15 +0800

    Transform first screen to spash screen

--- app/src/main/java/com/example/proflama/LaunchScreenActivity.java
+++ app/src/main/java/com/example/proflama/LaunchScreenActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -13,6 +14,8 @@ import android.widget.ImageView;
 public class LaunchScreenActivity extends AppCompatActivity {
 
     private ImageView lamaIcon;
+
+    private static final int DELAY = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +33,16 @@ public class LaunchScreenActivity extends AppCompatActivity {
                 openMainScreen();
             }
         });
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                openMainScreen();
+                finish();
+            }
+        };
+
+        new Handler().postDelayed(runnable, DELAY);
     }
 
     public void openMainScreen() {
