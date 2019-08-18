f7257e0ed2918a8aae207b931865850a233f8596
Philippe BENOIT <pbenoit@palo-it.com>
2019-08-18 20:29:38 +0800

    Clean splash screen

--- app/src/main/java/com/example/proflama/LaunchScreenActivity.java
+++ app/src/main/java/com/example/proflama/LaunchScreenActivity.java
@@ -13,8 +13,6 @@ import android.widget.ImageView;
 
 public class LaunchScreenActivity extends AppCompatActivity {
 
-    private ImageView lamaIcon;
-
     private static final int DELAY = 1500;
 
     @Override
@@ -22,27 +20,15 @@ public class LaunchScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_screen);
 
-        lamaIcon = findViewById(R.id.lamaIcon);
-        lamaIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openMainScreen();
-            }
-        });
-
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                openMainScreen();
-                finish();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);                finish();
             }
         };
 
         new Handler().postDelayed(runnable, DELAY);
     }
 
-    public void openMainScreen() {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
 }
