67e52063bf7d81c6d8f1612762ae836ce28987b1
Philippe BENOIT <pbenoit@palo-it.com>
2019-10-30 11:42:53 +0700

    Clean LocalNotifations

--- app/src/main/java/fr/pbenoit/proflama/notifications/LocalNotifications.java
+++ app/src/main/java/fr/pbenoit/proflama/notifications/LocalNotifications.java
@@ -15,6 +15,16 @@ public class LocalNotifications {
 
     private static final String CHANNEL_ID = "channel_prof_lama";
 
+    public static void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, ProfLama.getAppContext().getString(R.string.app_name), importance);
+            channel.setDescription("Prof Lama notifications");
+            NotificationManager notificationManager = ProfLama.getAppContext().getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
     public static void sendWorkCreationNotification(PendingIntent pendingIntent, String title) {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(ProfLama.getAppContext(), CHANNEL_ID)
                 .setSmallIcon(R.mipmap.ic_launcher)
@@ -26,50 +36,35 @@ public class LocalNotifications {
 
         android.app.NotificationManager notificationManager =
                 (android.app.NotificationManager) ProfLama.getAppContext().getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(0 /* ID of notification */, builder.build());
+        notificationManager.notify(0, builder.build());
     }
 
     public static void sendDailyReminderNotification(PendingIntent pendingIntent) {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(ProfLama.getAppContext(), CHANNEL_ID)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle(ProfLama.getAppContext().getString(R.string.app_name))
-                .setContentText("Poświęć 5 minut na przejrzenie kilku słów ;)")
+                .setContentText(ProfLama.getAppContext().getString(R.string.dailyNotificationContent))
                 .setContentIntent(pendingIntent)
                 .setOnlyAlertOnce(true)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
         android.app.NotificationManager notificationManager =
                 (android.app.NotificationManager) ProfLama.getAppContext().getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(1 /* ID of notification */, builder.build());
+        notificationManager.notify(1, builder.build());
     }
 
     public static void sendWeekSummaryNotification(PendingIntent pendingIntent, int numberOfWord) {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(ProfLama.getAppContext(), CHANNEL_ID)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle(ProfLama.getAppContext().getString(R.string.app_name))
-                .setContentText("Brawo, nauczyłeś się " + numberOfWord + " nowych słów w tym tygodniu")
+                .setContentText(ProfLama.getAppContext().getString(R.string.weeklyNotificationContent, numberOfWord))
                 .setContentIntent(pendingIntent)
                 .setOnlyAlertOnce(true)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
         android.app.NotificationManager notificationManager =
                 (android.app.NotificationManager) ProfLama.getAppContext().getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(2 /* ID of notification */, builder.build());
+        notificationManager.notify(2, builder.build());
     }
 
-    public static void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            //CharSequence name = getString(R.string.channel_name);
-            //String description = getString(R.string.channel_description);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Tltle A", importance);
-            channel.setDescription("Description D");
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = ProfLama.getAppContext().getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
-    }
 }
